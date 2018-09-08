28361ccb45e3ffc0974b56953490812a82cce2e2
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2018-09-08 08:59:11 +0200

    Build HomeVC User Interface

---
+++ htchhkr-development/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  htchhkr-development
+//
+//  Created by sade on 2/9/18.
+//  Copyright © 2018 sade. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
